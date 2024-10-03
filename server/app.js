240c282a8106d06b92234b64e386de0e27a70c41
vimalds15 <antovimalands@gmail.com>
2024-10-03 22:17:30 +0530

    feat: "health check"

--- server/app.js
+++ server/app.js
@@ -12,6 +12,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/',(req,res)=>{
+    return res.status(200).json({message:"Yes, API is working!!"})
+})
+
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/company", companyRoutes);
