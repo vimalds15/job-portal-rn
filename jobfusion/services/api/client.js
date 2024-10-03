fa3995e2ee489c4f07029fa39d6281070fbe1394
vimalds15 <antovimalands@gmail.com>
2024-10-03 22:26:43 +0530

    feat: "changed api url"

--- jobfusion/services/api/client.js
+++ jobfusion/services/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const apiClient = axios.create({
-  baseURL: "http://192.168.20.227:5000/api",
+  baseURL: "https://job-portal-rn.vercel.app/api",
   headers: {
     "Content-Type": "application/json",
   },
